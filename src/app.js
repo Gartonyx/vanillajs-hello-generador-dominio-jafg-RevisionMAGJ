5ebc50e6e32af8da713352b79d120a70afe0a453
Josue Frayre <josuefraygarcia@gmail.com>
2025-12-16 02:52:32 +0000

    generador de dominio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function randomDominio(){
+    const dominios = [];
+    let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
+    let ext = ['.com', '.net', '.org','.dev'];
+
+    for(let i = 0 ; i < pronoun.length ; i++ ){
+      const p = pronoun[i]
+      for(let j = 0; j < adj.length; j++){
+        const a = adj[j]
+        for(let k = 0;k < noun.length; k++){
+          const n = noun[k]
+          for(let l = 0; l < ext.length;l++){
+            const e = ext[l]
+
+            dominios.push(p+a+n+e)
+
+
+          }
+        }
+      }
+    }
+    return dominios
+  }
+  let dominio = document.getElementById("yellow")
+  dominio.innerHTML = randomDominio().join('<br>');
+
+  
 };
